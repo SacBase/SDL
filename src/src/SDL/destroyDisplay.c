8222e88b4e4e4dc0a77d1b61a7dda4ab3fd34db2
Stephan Herhut <sah@sac-home.org>
2006-11-13 16:10:42 +0000

    objetized SDL even more. objectizing Dislin

--- src/src/SDL/destroyDisplay.c
+++ src/src/SDL/destroyDisplay.c
@@ -2,14 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void destroyDisplay( SAC_ND_PARAM_out( scalres_nt, int),
-                     SAC_ND_PARAM_in_nodesc( disp_nt, Display))
+void destroyDisplay( SAC_ND_PARAM_in_nodesc( disp_nt, Display))
 {
-  SAC_ND_DECL__DATA( scalar_nt, int, );
-  SAC_ND_DECL__DESC( scalar_nt, );
-
-  SAC_ND_CREATE__SCALAR__DATA( scalar_nt, 0);
-
   if (SDLsac_eventhandler != NULL) {
     SDL_KillThread( SDLsac_eventhandler);
   }
@@ -21,6 +15,4 @@ void destroyDisplay( SAC_ND_PARAM_out( scalres_nt, int),
   if (SDLsac_timer != NULL) {
     SDL_RemoveTimer( SDLsac_timer);
   }
-
-  SAC_ND_RET_out( scalres_nt, scalar_nt);
 }
