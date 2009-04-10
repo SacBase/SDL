d4f3a2bada7eefc31946b0cc00423eaa0209ebdb
Carl Joslin <caj@sac-home.org>
2009-04-10 18:49:40 +0000

    New tag notation.

--- src/src/SDL/SDLsac.h
+++ src/src/SDL/SDLsac.h
@@ -13,13 +13,13 @@ extern SDL_TimerID SDLsac_timer;
 
 extern bool SDLsac_isasync;
 
-#define disp_nt (disp, (SCL, (HID, (NUQ,))))
-#define disp_nt (disp, (SCL, (HID, (NUQ,))))
-#define shp_nt  (shp, (AKD, (NHD, (NUQ,))))
-#define ar_nt   (ar, (AKD, (NHD, (NUQ,))))
-#define color_nt (col, (AKS, (NHD, (NUQ,))))
-#define scalar_nt (scl, (SCL, (NHD, (NUQ,))))
-#define scalres_nt (sclres, (SCL, (NHD, (NUQ,))))
-#define async_nt (async, (SCL, (NHD, (NUQ,))))
+#define disp_nt (disp, T_OLD((SCL, (HID, (NUQ,)))))
+#define disp_nt (disp, T_OLD((SCL, (HID, (NUQ,)))))
+#define shp_nt  (shp, T_OLD((AKD, (NHD, (NUQ,)))))
+#define ar_nt   (ar, T_OLD((AKD, (NHD, (NUQ,)))))
+#define color_nt (col, T_OLD((AKS, (NHD, (NUQ,)))))
+#define scalar_nt (scl, T_OLD((SCL, (NHD, (NUQ,)))))
+#define scalres_nt (sclres, T_OLD((SCL, (NHD, (NUQ,)))))
+#define async_nt (async, T_OLD((SCL, (NHD, (NUQ,)))))
 
 #endif
