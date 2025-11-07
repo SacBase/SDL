19e02ebd328ada6d788ef7527ae84f8fb00188e2
Jordy Aaldering <aalderingjordy@gmail.com>
2025-11-07 16:00:19 +0100

    it works standalone, but not from sac. ugh

--- src/src/sdl3.c
+++ src/src/sdl3.c
@@ -1,41 +1,108 @@
+#include <assert.h>
+#include <stdio.h>
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-#include "sac.h"
+//#include "sac.h"
 
-typedef struct SDLdisplay {
-    SDL_Window *window;
-    SDL_Surface *surf;
-} SDLdisplay;
+SDL_Window *window = NULL;
+SDL_Surface *surface = NULL;
+SDL_Mutex *mutex = NULL;
 
-SDLdisplay initDisplay(int w, int h)
+// typedef struct SDLdisplay {
+//     SDL_Window *window;
+//     SDL_Renderer *renderer;
+//     SDL_Surface *surface;
+//     SDL_Mutex *mutex;
+// } SDLdisplay;
+
+void initDisplay(int w, int h)
 {
-    SDLdisplay disp;
+    assert(window == NULL);
+    assert(surface == NULL);
+    assert(mutex == NULL);
 
     if (!SDL_Init(SDL_INIT_VIDEO))
     {
-        SAC_RuntimeError("SDL_Init failed: %s", SDL_GetError());
+        printf("SDL_Init failed: %s\n", SDL_GetError());
     }
 
-    SDL_Delay(2000);
-
-    disp.window = SDL_CreateWindow("SDL3", w, h, 0);
-    if(disp.window == NULL)
+    printf("a\n");
+    window = SDL_CreateWindow("SDL3", w, h, 0);
+    if(window == NULL)
     {
-        SAC_RuntimeError("SDL_CreateWindow failed: %s", SDL_GetError());
+        printf("SDL_CreateWindow failed: %s\n", SDL_GetError());
+    }
+
+    printf("b\n");
+
+    assert(window != NULL);
+    surface = SDL_GetWindowSurface(window);
+    if(surface == NULL)
+    {
+        printf("SDL_GetWindowSurface failed: %s\n", SDL_GetError());
+    }
+
+    SDL_ClearSurface(surface, 0., 0., 120., 255.);
+    SDL_UpdateWindowSurface(window);
+
+    printf("c\n");
+
+
+
+    SDL_Delay(2000);
+
+    mutex = SDL_CreateMutex();
+    if(mutex == NULL)
+    {
+        printf("SDL_CreateMutex failed: %s\n", SDL_GetError());
     }
 
     SDL_Delay(2000);
-
-    disp.surf = SDL_GetWindowSurface(disp.window);
-
-    SDL_Delay(2000);
-
-    return disp;
 }
 
-void closeDisplay(SDLdisplay disp)
+void drawPixel(int x, int y, int r, int g, int b)
 {
-    SDL_DestroySurface(disp.surf);
-    SDL_DestroyWindow(disp.window);
+    printf("drawing pixel at %d %d\n", x, y);
+
+    SDL_LockMutex(mutex);
+
+    if (SDL_MUSTLOCK(surface)) {
+        if (!SDL_LockSurface(surface)) {
+            printf("SDL_LockSurface failed: %s\n", SDL_GetError());
+        }
+    }
+
+    int yoffset = x * (surface->pitch / 4);
+    int xoffset = y;
+
+    Uint32 *bptr = (Uint32 *) (surface->pixels) + xoffset + yoffset;
+    *bptr = r << 16 | g << 8 | b;
+
+    if (SDL_MUSTLOCK(surface)) {
+        SDL_UnlockSurface(surface);
+    }
+
+    SDL_UpdateWindowSurface(window);
+
+    SDL_UnlockMutex(mutex);
+}
+
+
+void closeDisplay(void)
+{
+    printf("SDL_Quit\n");
     SDL_Quit();
 }
+
+#if 0
+int main()
+{
+    initDisplay(640, 480);
+    printf("Created window\n");
+    drawPixel(50,50, 255,255,255);
+    drawPixel(60,60, 0,0,0);
+    closeDisplay();
+    return 0;
+}
+#endif
